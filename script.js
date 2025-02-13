deca1a97f522c2d98763d3523fe1922c9c0e3860
MattiaDV <mattiadv25105@gmail.com>
2025-02-13 20:27:31 +0100

    jkhgkjhgkjhgkjh

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ videoPart.id = "videoLoad";
 videoPart.classList.add("videoLoaded");
 videoPart.width = 400;
 videoPart.height = 320;
-videoPart.setAttribute('controls');
 videoPart.setAttribute("src", "https://www.youtube.com/embed/NLi2v-Gq-5A");
 videoPart.setAttribute("frameborder", "0");
 videoPart.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture");
