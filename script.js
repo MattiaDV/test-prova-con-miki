e63ac3f4985cb593ae618d885b05323942d46c15
MattiaDV <mattiadv25105@gmail.com>
2025-02-13 20:20:16 +0100

    jkhgkjhgkjhgkjh

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let createPart = document.getElementById('jsCreatePart');
 let videoPart = document.createElement('video');
 videoPart.id = "videoLoad";
 videoPart.classList.add = "videoLoaded";
-videoPart.src = "https://www.youtube.com/watch?v=NLi2v-Gq-5A&list=RDNLi2v-Gq-5A&start_radio=1";
+videoPart.width = "400px";
+videoPart.height = "320px";
 
 let video = document.createElement("source");
 video.src = "https://www.youtube.com/watch?v=NLi2v-Gq-5A&list=RDNLi2v-Gq-5A&start_radio=1";
