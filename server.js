40dea902ca10efa54ec1c862a9cfdb951a2a8b4c
MattiaDV <mattiadv25105@gmail.com>
2025-02-15 10:58:33 +0100

    jkhgkjhgkjhgkjh

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const { readFile } = require('fs/promises');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-    res.end();
+/* app.get('/', async (req, res) => {
+    try {
+        let htmlContent = await readFile('index.html', 'utf-8');
+        res.send(htmlContent);
+    } catch(err) {
+        console.log("errore");
+        res.send(err);
+    }
+}); */
+
+app.use(express.static('./'));
+
+app.get('/', async (req, res) => {
+    try {
+        let htmlContent = await readFile('index.html', 'utf-8');
+        res.send(htmlContent);
+    } catch (err) {
+        console.error("Errore nel leggere il file:", err);
+        res.status(500).send("Errore interno del server");
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server in ascolto su http://localhost:${PORT}`);
+app.listen(PORT, '192.168.1.18', () => {
+    console.log(`Server in ascolto su http://192.168.1.18:${PORT}`);
 });
